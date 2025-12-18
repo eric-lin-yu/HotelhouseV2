153164f473b24f6eac39aad7ed09b0ac68453564
EricLin <zl2408399@gmail.com>
2025-12-18 14:52:11 +0800

    add data extension prettyJsonString

--- 寶島旅社/Common/Utility/Extension/Data+Extension.swift
+++ 寶島旅社/Common/Utility/Extension/Data+Extension.swift
@@ -28,4 +28,13 @@ extension Data {
         self = data
     }
     
+    /// 將 Data 轉換為美化格式的 JSON 字串，用於日誌輸出。
+    var prettyJsonString: String {
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted, .sortedKeys]),
+              let prettyPrintedString = String(data: data, encoding: .utf8) else {
+            return String(data: self, encoding: .utf8) ?? "Invalid Data"
+        }
+        return prettyPrintedString
+    }
 }
